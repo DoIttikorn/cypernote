69bc04075bf35281d5a06bdaac59439bad4f4a92
ittikorn chawkamud <aseaways1234@gmail.com>
2023-02-08 16:49:45 +0700

    feat: add echo

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(200, "Hello, World!")
 }
